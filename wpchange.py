26c2e728d63773e079b0e3086d8c3aa0030126c9
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2016-05-11 13:22:19 -0300

    New configuration file and class.

--- wpchange.py
+++ wpchange.py
@@ -1,1 +1,35 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+
+import os
+import sys
+import ConfigParser
+
+class utils():
+	def __init__(self):
+		pass
+
+class loadConfProject():
+    def __init__(self, pathFile):
+        self.pathFile = pathFile
+        self.Config = ConfigParser.ConfigParser()
+        self.Config.read(self.pathFile)
+
+    def getSections(self):
+        return self.Config.sections()
+
+    def getPathProject(self, project):
+        return self.Config.get(project, 'path')
+
+    def getLogProject(self, project):
+        return self.Config.get(project, 'log')
+
+
+def main():
+    a = loadConfProject('wpchange.conf')
+    print a.getSections()
+    for i in a.getSections():
+        print a.getPathProject(i)
+        print a.getLogProject(i)
+
+if __name__ == "__main__":
+	main()
